05724efe63367827a53796fa89dfd437a8fd2c0d
sparethme <spare@ualberta.ca>
2020-05-28 16:56:28 -0400

    Updates

---
+++ precache-manifest.f4af03f32152e831dcbdf9ef9eca5e32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbd1ce0fa58e6c8404f6562b9cbfac5",
+    "url": "/sparethme/keeperapp.git/index.html"
+  },
+  {
+    "revision": "368b64bd8f61fd3222aa",
+    "url": "/sparethme/keeperapp.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "72ca46556e8f656d474b",
+    "url": "/sparethme/keeperapp.git/static/js/2.b45fe1ec.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sparethme/keeperapp.git/static/js/2.b45fe1ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "368b64bd8f61fd3222aa",
+    "url": "/sparethme/keeperapp.git/static/js/main.c4d911d1.chunk.js"
+  },
+  {
+    "revision": "06f4596a4a920469e811",
+    "url": "/sparethme/keeperapp.git/static/js/runtime-main.c581a72c.js"
+  }
+]);
